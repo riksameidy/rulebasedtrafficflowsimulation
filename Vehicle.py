748e493a7ea6f277cc29d563e19a909eac3c2899
Riksa Meidy Karim <riksameidy@Riksa-Meidy-Macbook-Air.local>
2019-11-19 11:06:04 +0700

    add vparam

--- Vehicle.py
+++ Vehicle.py
@@ -4,6 +4,15 @@ class Vehicle:
         self.v = v * 1000/3600
         self.posisi = posisi
 
+    def update_speed(self,v):
+        self.v = self.v + (v * 1000/3600 )
+
+    def set_id(self,id):
+        self.id = id
+
+    def setVparams(self,vparams):
+        self.vparams = vparams
+
     def printVehicle(self):
         print('type = ' + str(self.type) + ', v = ' + str(self.v * 3600/1000) + ' km/jam , Posisi (x) = ' + str(self.posisi.x), ', Lane = ' + str(self.posisi.lane) )
 
@@ -12,3 +21,16 @@ class Posisi:
         self.x = x
         self.lane = lane
 
+class ParamState:
+    def __init__(self,C,discount):
+        self.C = C
+        self.discount = discount
+
+class Vparams:
+    def __init__(self,v0,v1,v2,v3):
+        self.v0 = v0
+        self.v1 = v1
+        self.v2 = v2
+        self.v3 = v3
+    def printVparams(self):
+        print('v0 = ' + str(self.v0)  + ' ' + 'v1 = ' + str(self.v1)  + ' ', 'v2 = ' + str(self.v2)  + ' ', 'v3 = ' + str(self.v3)  + ' ')
--- main_serial.py
+++ main_serial.py
@@ -19,8 +19,9 @@ arrival_ratio = {
     "medium" : 2,
     "small" : 6
 }
-t_akhir = 7200
+t_akhir = 600
 panjang_tol = 144 * 10**3 # meter
+id_generator = 0
 # ======================================================================================
 
 def prob_poission(lamda,k):
@@ -68,6 +69,92 @@ def create_vehicle(prob_arrival,ratio,v_params):
 
         return Vehicle( type,v, Posisi(0,1) )
 
+characteristic_distance = {
+    "affected": None,
+    "safety":None,
+    "feasible_passing":None,
+    "critical":None,
+    "extreme":None
+}
+
+
+def speed_rule_travelling_freely(mobil):
+    v = np.random.uniform(-3,3)
+    return  mobil.update_speed(v)
+
+def split_mobil_per_lane(arr_mobil):
+    left_lane = []
+    right_lane = []
+    for mobil in arr_mobil:
+        if(mobil.posisi.lane == 1):
+            left_lane.append(mobil)
+        else:
+            right_lane.append(mobil)
+    return ( left_lane, right_lane)
+
+def generate_vparams_mobil(mobil,left_lane,right_lane):
+    lane_mobil = mobil.posisi.lane
+    if(lane_mobil==1):
+        lane = left_lane
+        opp_lane = right_lane
+    else:
+        lane = right_lane
+        opp_lane = right_lane
+
+    idx_mobil = -1
+    for i,m in enumerate(lane):
+        if(mobil.id == m.id):
+            idx_mobil = i
+
+    if(idx_mobil == -1):
+        return None
+
+    idx_next = -1
+    idx_prev = -1
+    idx_adj_prev= -1
+    idx_adj_next = -1
+    idx_opp_prev= -1
+    idx_opp_next = -1
+    v0 = None
+    v1 = None
+    v2 = None
+    v3 = None
+
+    for i,m in enumerate(opp_lane):
+        if( m.posisi.x > mobil.posisi.x ):
+            if(i-1 > 0 ):
+                idx_prev = i-1
+            else:
+                idx_prev =  -1
+
+            if i< len(right_lane):
+                idx_next = i
+            else:
+                idx_next = -1
+
+    # check if in boundary adj lane
+    if(idx_mobil+1 < len(lane) ):
+        idx_adj_next = idx_mobil+1
+        v2 = lane[idx_adj_next].v
+
+    if(idx_mobil-1 > 0):
+        idx_adj_prev = idx_mobil -1
+        v0 = lane[idx_adj_prev].v
+
+    # check if in boundary ops lane
+    if(idx_next!=-1):
+        idx_opp_next = idx_next
+        v3 = opp_lane[idx_opp_next].v
+
+    if(idx_prev!=-1):
+        idx_opp_prev = idx_prev
+        v1 = opp_lane[idx_opp_prev].v
+
+    mobil.setVparams( Vparams(v0,v1,v2,v3) )
+
+
+
+
 # Main Simulation
 step = int(t_akhir/delta_t)
 arr_mobil = []
@@ -76,5 +163,13 @@ for i in range(step):
     # 1. Cek Arrival Mobil Baru dari pintu masuk Tol
     mobil = create_vehicle(prob_arrival, arrival_ratio, v_params)
     if(mobil):
+        id_generator = id_generator + 1
+        mobil.set_id(id_generator)
         arr_mobil.append(mobil)
     # 2. Update Posisi Mobil Sesuai Rule
+    (left_lane,right_lane) = split_mobil_per_lane(arr_mobil)
+    for m in arr_mobil:
+        generate_vparams_mobil(m,left_lane,right_lane)
+        m.printVehicle()
+        m.vparams.printVparams()
+
